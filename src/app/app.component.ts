205e889538ff89b971f810341efb4d5155d20c99
Itallo Ramon <itallostudy@gmail.com>
2024-09-03 17:58:27 -0300

    feat: create portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule, TranslateModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
+  faSun = faSun;
+  faMoon = faMoon;
+  isDarkMode: boolean = false;
+
+  constructor(public translate: TranslateService) {
+    translate.setDefaultLang('en');
+    translate.use('en');
+    this.applyTheme();
+  }
+
+  toggleTheme(): void {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark', this.isDarkMode);
+    localStorage.setItem('theme', this.isDarkMode ? 'dark' : 'light');
+  }
+
+  applyTheme(): void {
+    this.isDarkMode = localStorage.getItem('theme') === 'dark';
+    document.body.classList.toggle('dark', this.isDarkMode);
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+  }
 }
